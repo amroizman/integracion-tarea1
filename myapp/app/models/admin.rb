d2bd36895ca754ac1eb654b51db07b57dc09b934
Andrea Roizman <AndreaRoizman@MacBook-Pro-de-Andrea-6.local>
2018-03-14 17:00:28 -0300

    Added validations to models

--- myapp/app/models/admin.rb
+++ myapp/app/models/admin.rb
@@ -1,2 +1,8 @@
 class Admin < ApplicationRecord
+  validates :nombre,
+            presence: true
+
+  validates :password,
+            presence: true,
+            length: {minimum: 4}
 end
--- myapp/app/models/article.rb
+++ myapp/app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
   has_many :comments
+
+  validates :titulo,
+            presence: true
+
+  validates :bajada,
+            presence: true,
+            length: {maximum: 200}
+  validates :cuerpo,
+            presence: true
+            
 end
