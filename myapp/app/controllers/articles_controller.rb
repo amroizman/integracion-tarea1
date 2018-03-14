3abb4a85d6d094988fa2bd0f54d42e48005c2024
Andrea Roizman <AndreaRoizman@MacBook-Pro-de-Andrea-6.local>
2018-03-14 15:20:32 -0300

    Updated index and added order/limit of articles

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.order("created_at DESC").limit(10)
   end
 
   # GET /articles/1
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :admins
   resources :comments
+
+  root to: "articles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
